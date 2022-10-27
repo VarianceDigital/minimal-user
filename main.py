5971c130baf646a505342c4f52e655e27247e1f2
RinaldoNani <60207094+RinaldoNani@users.noreply.github.com>
2022-10-27 13:14:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 os.environ["SESSION_SECRET"]="YourSessionSecret"
 os.environ["JWT_SECRET_HTML"]="YourHTMLApiSecret"
-os.environ["JWT_MAILER_SECRET"]="YourHTMLApiSecret"
+os.environ["JWT_MAILER_SECRET"]="YourMailerSecret"
 os.environ["MAILER_URL"]="https://mymailer.sass.com/"
 os.environ["AWS_TILES_BUCKET_NAME"]="my-tiles" #for user tiles
 os.environ["AWS_TILES_BUCKET_URL"]="https://my-tiles.s3.eu-west-3.amazonaws.com/" 
